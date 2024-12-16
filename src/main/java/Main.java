ef5dc7dc7bd05f1590bbf7964ee0cefeb03541d9
César David Rueda Daza <ruedacesardavid@gmail.com>
2024-12-16 12:00:42 -0500

    Mi proyecto calibrador

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+import org.example.Calibrador;
+
+public class Main {
+
+    public static void main(String[] args){
+        Calibrador objeto= new Calibrador();
+        objeto.setVisible(true);
+    }
+}
